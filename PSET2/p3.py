88cd480f8452bf0b53351f60b5bfaf45ab6e6faf
jfu006 <jfu006@ucr.edu>
2018-10-18 10:02:32 -0700

    added sklearn normalization in comments

--- PSET2/p3.py
+++ PSET2/p3.py
@@ -2,6 +2,7 @@ import numpy as np
 from numpy import unravel_index
 from p1 import gridWorld
 import p2 as reward
+# from sklearn import preprocessing
 
 class policyGrid():
 	def __init__(self):
@@ -83,8 +84,84 @@ class policyGrid():
 				self.policy_eval[i] += (self.gw.probability(self.gw.S[i],self.policy[i],0.1,self.gw.getNextState(0.1,self.gw.S[i],self.policy[i]))*(reward.get_reward(gw.S[i]) + lamda*self.olicy_eval[i]))
 
 			thresh = abs(sum(self.policy_eval - thresh_matrix)/len(self.gw.S))
+			# thresh = preprocessing.normalize([self.policy_eval - thresh_matrix])
 
 		return self.policy_eval
+
+
+
+	def plot_trajectory(self, s, policy = None, Pe= 0):
+		'''
+			Function to plot trajectory given a state
+		'''
+		h_angle_dict = { 0: 90, 1:60, 2:30, 3:0, 4:330, 5:300, 6:270, 7:240, 8:210, 9:180, 10:150, 11:120  }
+		def delta_coord(angle):
+		    '''
+		    angle - Angle you want your end point at in degrees.
+		    '''
+		    length = 0.45
+		    
+		    # find delta
+		    deltax = length * math.cos(math.radians(angle))
+		    deltay = length * math.sin(math.radians(angle))
+		        
+		    return deltax, deltay
+
+		if policy == None:
+			policy = self.policy
+
+
+		S = np.array(self.gw.S)
+		reward = False
+		X = [s[0]]
+		Y = [s[1]]
+
+		u, v = delta_coord(h_angle_dict[s[2]])
+		U = [u]
+		V = [v]
+
+		while(not(reward)):
+			s_index = np.where((S == s).all(axis=1))
+			print s_index
+			a = policy[s_index[0][0]]
+			s = self.gw.getNextState(Pe, s, a)
+
+			X.append(s[0])
+			Y.append(s[1])
+
+			u, v = delta_coord(h_angle_dict[s[2]])
+			U.append(u)
+			V.append(v)
+
+			if s[0] == self.rg.goal[0] and s[1] == self.rg.goal[1]:
+				reward = True
+			
+
+		plt.figure(figsize=(8,8))
+		ax = plt.gca();
+
+		ax.quiver(X[0], Y[0], U[0], V[0], color=['r'],angles='xy', scale_units='xy', scale = 1 )
+		ax.quiver(X[1:], Y[1:], U[1:], V[1:], angles='xy', scale_units='xy', scale = 1)
+
+		# Major ticks
+		ax.set_xticks(np.arange(0, 6, 1));
+		ax.set_yticks(np.arange(0, 6, 1));
+
+		# Labels for major ticks
+		ax.set_xticklabels(np.arange(0, 6, 1));
+		ax.set_yticklabels(np.arange(0, 6, 1));
+
+		# Minor ticks
+		ax.set_xticks(np.arange(-.5, 6, 1), minor=True);
+		ax.set_yticks(np.arange(-.5, 6, 1), minor=True);
+
+
+		# Gridlines based on minor ticks
+		ax.grid(which='minor', linestyle='-', linewidth=2)
+
+
+# get_policy_eval(p,d,Pe,lamda)
+# plot_trajectory(s, policy = None, Pe= 0)
 
 
 		
