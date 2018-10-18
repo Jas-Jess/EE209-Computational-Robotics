30fc639d66466014546a3e82bb6cf6e26b4b850b
JasmineM <jmore021@ucr.edu>
2018-10-18 01:23:23 -0700

    Finished 1b & c

--- PSET2/p3.py
+++ PSET2/p3.py
@@ -1,6 +1,8 @@
+import matplotlib.pyplot as plt
 from p1 import GridWorld2D
 from p2 import RewardGrid
 import numpy as np 
+import math
 
 '''
 	This is the policy Iteration class specifically for the gridworld  
@@ -86,6 +88,79 @@ class PolicyIteration():
 
 		self.policy = get_initial_policy()
 		# END OF INIT FUNCTION
+
+
+	def plot_trajectory(self, s, policy = None, Pe= 0):
+		'''
+			Function to plot trajectory given a state
+		'''
+		h_angle_dict = { 0: 90, 1:60, 2:30, 3:0, 4:330, 5:300, 6:270, 7:240, 8:210, 9:180, 10:150, 11:120  }
+		def delta_coord(angle):
+		    '''
+		    angle - Angle you want your end point at in degrees.
+		    '''
+		    length = 0.45
+		    
+		    # find delta
+		    deltax = length * math.cos(math.radians(angle))
+		    deltay = length * math.sin(math.radians(angle))
+		        
+		    return deltax, deltay
+
+		if policy == None:
+			policy = self.policy
+
+
+		S = np.array(self.gw.S)
+		reward = False
+		X = [s[0]]
+		Y = [s[1]]
+
+		u, v = delta_coord(h_angle_dict[s[2]])
+		U = [u]
+		V = [v]
+
+		while(not(reward)):
+			s_index = np.where((S == s).all(axis=1))
+			print s_index
+			a = policy[s_index[0][0]]
+			s = self.gw.get_next_state(Pe, s, a)
+
+			X.append(s[0])
+			Y.append(s[1])
+
+			u, v = delta_coord(h_angle_dict[s[2]])
+			U.append(u)
+			V.append(v)
+
+			if s[0] == self.rg.goal[0] and s[1] == self.rg.goal[1]:
+				reward = True
+			
+
+		plt.figure(figsize=(8,8))
+		ax = plt.gca();
+
+		ax.quiver(X[0], Y[0], U[0], V[0], color=['r'],angles='xy', scale_units='xy', scale = 1 )
+		ax.quiver(X[1:], Y[1:], U[1:], V[1:], angles='xy', scale_units='xy', scale = 1)
+
+		# Major ticks
+		ax.set_xticks(np.arange(0, 6, 1));
+		ax.set_yticks(np.arange(0, 6, 1));
+
+		# Labels for major ticks
+		ax.set_xticklabels(np.arange(0, 6, 1));
+		ax.set_yticklabels(np.arange(0, 6, 1));
+
+		# Minor ticks
+		ax.set_xticks(np.arange(-.5, 6, 1), minor=True);
+		ax.set_yticks(np.arange(-.5, 6, 1), minor=True);
+
+
+		# Gridlines based on minor ticks
+		ax.grid(which='minor', linestyle='-', linewidth=2)
+
+
+
 
 
 
