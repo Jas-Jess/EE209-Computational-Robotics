2b60e71fda3658cfbc36852cef736106ad0b6649
jfu006 <jfu006@ucr.edu>
2018-10-20 19:53:59 -0700

    Add files via upload

--- PSET2/p1.py
+++ PSET2/p1.py
@@ -1,129 +1,32 @@
-import numpy as np 
+import numpy as np
 import itertools
 import random
 
-class GridWorld2D():
-	# Headings
-	H = list(range(0,12))
-	# Action space
-	A = [['forward', 'right'], ['forward', 'left'], 
-	     ['backward', 'right'], ['backward', 'left'], 
-	     ['forward', 'no rotation'], ['backward', 'no rotation'],
-	     ['no action', '']]
+class gridWorld:
+	rotation = list(range(0,12))
+	action = [['forward','right'],['forward','left'],['forward','none'],['backward','right'],['backward','left'],['backward','none'],['none','none']]
 
-	'''
-	 *****************
-	 PUBLIC FUNCTIONS
-	 *****************
-	'''
-	def __init__(self, L, W, Pe = 0, initial_state = None):
+	def __init__(self, L, W, Pe=0):
 		self.L = L
 		self.W = W
 		self.Pe = Pe
 
-		# Creating the state space (x,y, h)
-		self.S = self.__create_state_space(L, W)
-
-		# Randomly chooses intial state (x, y, h)
-		if initial_state == None:
-			self.s = list(self.S[random.randint(0,len(self.S)-1)])
-		else:
-			self.s = list(initial_state)
-
-	def user_defined_action(self,movement, rotation):
-		''' 
-			This function takes action and updates current state. 
-			Inputs:
-			 	movement = 'forward', 'backward', 'no action' 
-			 	rotation = 'right', 'left', 'no rotation'
-
-		'''
-		if movement == 'no action':
-			rotation = ''
-
-		self.__action([movement, rotation])
-		return
-
-	def random_action(self):
-		'''
-			Robot randomly picks an action
-		'''
-		self.__action(self.A[random.randint(0,len(self.A)-1)])
-		return
-
-
-	def transition_probability(self, Pe, s, a, s_next):
-		p_sa = 1
-		# if no action is taken, staying in current state is 100%
-		if a[0] == 'no action' and s == s_next:
-			return(p_sa) 
-
-		# create possible state probability according to your action and state
-		# There is 1-2*Pe chance that the robot does the correct move/rotation
-		x,y,_ = self.__move(a[0], s)
-		h = self.__rotate(a[1], s[2])
-
-		if s_next == list([x, y, h]): return (1.0 - 2.0*Pe)
-
-		# There is Pe chance that the robot moves right first
-		h = self.__rotate('right', s[2])
-		x,y,_ = self.__move(a[0], list([s[0], s[1], h]))
-		h = self.__rotate(a[1], h)
-
-		if s_next == list([x, y, h]): return Pe
-
-		# There is Pe chance that the robot moves left first
-		h = self.__rotate('left', s[2])
-		x,y,_ = self.__move(a[0], list([s[0], s[1], h]))
-		h = self.__rotate(a[1], h)
-
-		if s_next == list([x, y, h]): return Pe
-
-		# Any other state will be zero
-		return 0
-
-	def get_next_state(self, Pe, s, a):
-		# if no action is taken, staying in current state is 100%
-		if a[0] == 'no action':
-			return(s)
-
-		# different chances of which movement first
-		first_action = np.random.choice(np.array(['movement','right', 'left']), 1, p = [1.0-2.0*Pe, Pe, Pe])
-
-		if first_action != 'movement': 
-			s[2] = self.__rotate(first_action, s[2])
-
-		s = list(self.__move(a[0], s))
-		s[2] = self.__rotate(a[1], s[2])
-
-		return s
-
-
-	'''
-	 *****************
-	 PRIVATE FUNCTIONS
-	 *****************
-	'''
+		#create state space
+		self.S = self.__create_state_space(L,W)
+		# Randomly chooses intial state
+		self.s = self.S[random.randint(0,len(self.S))]
 
 	def __create_state_space(self,L,W):
-		'''
-			PRIVATE FCN
-				creates the state space depending on L and W
-		'''
 		L_list = list(range(0,L))
 		W_list = list(range(0,W))
 
-		S_list = [L_list, W_list, self.H]
+		S_list = [L_list, W_list, self.rotation]
 
-		S_combination = list(list(itertools.product(*S_list)))
+		S_combination = list(itertools.product(*S_list))
 		return(S_combination)
 
 	def __action(self,a):
-		'''
-			PRIVATE FCN
-				provides action
-		'''
-		if a == ['no action', '']:
+		if a == ['none','none']:
 			return
 
 		# picks if movement is first or rotation
@@ -137,7 +40,6 @@ class GridWorld2D():
 
 
 		return
-
 
 	def __rotate(self,a, h = None):
 		'''
@@ -164,7 +66,7 @@ class GridWorld2D():
 	def __move(self,a, s = None):
 		'''
 			PRIVATE FCN
-				moves forward or backward
+				moves foward or backward
 		'''
 		if s == None:
 			x, y, h = self.s
@@ -205,11 +107,86 @@ class GridWorld2D():
 		return [x, y, h]
 
 
+	def user_defined_action(movement, rotation):
+		''' 
+			This function takes action and updates current state. 
+			Inputs:
+				movement = 'foward', 'backward', 'none' 
+				rotation = 'right', 'left', 'none'
+		'''
+		if movement == 'none':
+			rotation = 'none'
+
+		self.__action([movement, rotation])
+		return
+
+	def random_action():
+		'''
+			Robot randomly picks an action
+		'''
+		self.__action(self.action[random.randint(0,len(self.action))])
+		return
+
+	def probability(self,s0,a,Pe,s1):
+		'''
+			if s1 is out of range -> 0
+			if s0 == s1 -> 1-2*Pe
+			if s0 != s1 but is in range -> Pe
+		'''
+		#
+		if (a == ['none','none']) and (s0 == s1):
+			return 1
+		elif (abs(s1[0] - s0[0]) > 1) or (abs(s1[1] - s0[1]) > 1):
+			return 0
+		else:
+			sTemp = s0
+			#check which direction it's facing
+			if 2 <= s0[2] <= 4:	#right
+				if a[0] == ['forward']:
+					sTemp[0] = sTemp[0] + 1
+				elif a[0] == ['backward']:
+					sTemp[0] = sTemp[0] - 1
+			elif 5 <= s0[2] <= 7:	#down
+				if a[0] == ['forward']:
+					sTemp[1] = sTemp[1] - 1
+				elif a[0] == ['backward']:
+					sTemp[1] = sTemp[1] + 1
+			elif 8 <= s0[2] <= 10:	#left
+				if a[0] == ['forward']:
+					sTemp[0] = sTemp[0] - 1
+				elif a[0] == ['backward']:
+					sTemp[0] = sTemp[0] + 1
+			else:					#up
+				if a[0] == ['forward']:
+					sTemp[1] = sTemp[1] + 1
+				elif a[0] == ['backward']:
+					sTemp[1] = sTemp[1] - 1
 
 
+			#do rotation
 
+			if sTemp == s1:
+				return (1-2*Pe)
+			else:
+				return Pe
 
+	def getNextState(self, Pe, s, a):
+		# if no action is taken, staying in current state is 100%
+		
+		if a[0] == 'none':
+			return(s)
 
+		# different chances of which movement first
+		first_action = np.random.choice(np.array(['movement','right', 'left']), 1, p = [1.0-2.0*Pe, Pe, Pe])
+
+		if first_action != 'movement': 
+			s = [s[0], s[1], self.__rotate(first_action, s[2])]
+
+		
+		s = list(self.__move(a[0], s))
+		s = [s[0], s[1],self.__rotate(a[1], s[2])]
+
+		return s
 
 
 
